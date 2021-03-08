b8dbfa3366a0b595ca4f18441f7eb31943966789
Кравцов Виктор <viktor.kravtsov@gazprombank.ru>
2021-03-08 22:37:12 +0300

    submodule

---
+++ app/src/main/java/com/example/gitsubmodule/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.gitsubmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SumCalcTest {
+
+    @Test
+    fun AssertionsSuccessTest() {
+        var list = mutableMapOf<String, String>("totalCreditQty" to "10", "rate" to "15", "term" to "20")
+        val variable1 = "{{totalCreditQty}} {{rate}} * 100 / 12 / 1 1 {{rate}} 100 / 12 / + 1 {{term}} - ^ - / 0 round"
+        val variable2 = "1 2 +"
+        val variable3 = "1 2 * 3 + 2 ^"
+        val variable4 = "4.4567 2.0 round"
+
+        assertTrue(SumCalculationPostfixSolver.postfixExpression(variable1, list) == "1.0")
+        assertTrue(SumCalculationPostfixSolver.postfixExpression(variable2, list) == "3.0")
+        assertTrue(SumCalculationPostfixSolver.postfixExpression(variable3, list) == "25.0")
+        assertTrue(SumCalculationPostfixSolver.postfixExpression(variable4, list) == "4.46")
+    }
+
+    @Test
+    fun assertionsFailedTest() {
+        var list = mutableMapOf<String, String>("totalCreditQty" to "10", "rate" to "15", "term" to "20")
+        val variable1 = "{{totalCreditQty}} {{rate}} * 100 / 12 / 1 1 {{rate}} 100 / 12 / + 1 {{term}} - ^ - / 0 round"
+        val variable2 = "1 2 -"
+        val variable3 = "1 2 / 3 +"
+        val variable4 = "4.4567 2 round"
+
+        assertFalse(SumCalculationPostfixSolver.postfixExpression(variable1, list) == "2.0")
+        assertFalse(SumCalculationPostfixSolver.postfixExpression(variable2, list) == "1.0")
+        assertFalse(SumCalculationPostfixSolver.postfixExpression(variable3, list) == "3.6")
+        assertFalse(SumCalculationPostfixSolver.postfixExpression(variable4, list) == "4.45")
+    }
+}
